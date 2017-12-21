49ce48a701f68129f9490b58e6eea245a20dcb8f
itsjzt <saurabh.19@outlook.com>
2017-12-21 17:04:55 +0530

    hell still *.*

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 const apiUrl = (q) => `https://en.wikipedia.org/w/api.php?&origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${q}`
+const linkUrl = (q) => `https://en.wikipedia.org/wiki/${q.toString().replace(' ', '_')}`
 
-fetch(apiUrl('lol'))
-  .then(r => r.json())
-  .then(d => console.log(d))
+async function getjson(query) {
+  const blob = await fetch(apiUrl(query))
+  console.log(blob)
+}
 
+getjson('samsung')
