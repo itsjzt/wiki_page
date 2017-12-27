a9d0228c4608225166cdb61610e608948e978e12
itsjzt <saurabh.19@outlook.com>
2017-12-27 13:20:51 +0530

    almost done

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-
+// little helper functions (**)
 const apiUrl = (q) => `https://en.wikipedia.org/w/api.php?&origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${q}`
-const linkUrl = (q) => `https://en.wikipedia.org/wiki/${q.toString().replace(' ', '_')}`
+const linkUrl = (q) => `https://en.wikipedia.org/wiki/${q.toString().replace(' ', '_')}`.toLowerCase()
+const li = (link, title, extract) => `<li class="show_each"><a href="${link}" target="_blank"><h3 class="heading">${title} </h3> <p class="show_extract">${extract}</p> </a></li>`
 
-async function getjson(query) {
-  const blob = await fetch(apiUrl(query))
-  console.log(blob)
+// calling the next function with values
+function find(el) {
+  if (el) el.preventDefault();
+
+  const val = document.querySelector('#search').value
+  fetch(apiUrl(val), {mode: 'cors'})
+  .then( res => res.json() )
+  .then( json => trimmer(json.query.pages))
+  return false;
 }
 
-getjson('samsung')
+// triming the obj's and call next function
+function trimmer(resp) {
+  const arr = []
+  for (obj in resp) {
+    const { title, extract } = resp[obj]
+    arr.push({ title, extract, link: linkUrl(title) })
+  }
+  arr.map( n => inject('#wiki', n) )
+}
+
+// inject obj's in the dom.
+function inject(domChild, obj) {
+  const { link, title, extract } = obj
+  const str = li(link, title, extract)
+  document.querySelector(domChild).innerHTML += str
+}
+
+function submit(e) {
+  if (e.key === 'Enter') {
+    find()
+  }
+}
+
+// dom calls
+document.querySelector('form').addEventListener('submit', find)
+// after first time submit doesn't work
+document.querySelector('#search').addEventListener('keydown', submit)
