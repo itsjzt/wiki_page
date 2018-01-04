6a07b0a2c77b63cab30909e56e8419e887444c91
itsjzt <saurabh.19@outlook.com>
2018-01-04 16:28:57 +0530

    finisehed it 😄 👍

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const linkUrl = (q) => `https://en.wikipedia.org/wiki/${q.toString().replace(' '
 const li = (link, title, extract) => `<li class="show_each"><a href="${link}" target="_blank"><h3 class="heading">${title} </h3> <p class="show_extract">${extract}</p> </a></li>`
 
 // calling the next function with values
-function find(el) {
-  if (el) el.preventDefault();
-
+function find(e) {
   const val = document.querySelector('#search').value
   fetch(apiUrl(val), {mode: 'cors'})
   .then( res => res.json() )
-  .then( json => trimmer(json.query.pages))
-  return false;
+  .then( json => trimmer(json['query']['pages']))
 }
 
 // triming the obj's and call next function
 function trimmer(resp) {
   const arr = []
-  for (obj in resp) {
+  for (let obj in resp) {
     const { title, extract } = resp[obj]
     arr.push({ title, extract, link: linkUrl(title) })
   }
+  document.querySelector('#wiki').innerHTML = ''
   arr.map( n => inject('#wiki', n) )
 }
 
@@ -29,6 +27,7 @@ function inject(domChild, obj) {
   const { link, title, extract } = obj
   const str = li(link, title, extract)
   document.querySelector(domChild).innerHTML += str
+  return false;
 }
 
 function submit(e) {
@@ -37,7 +36,5 @@ function submit(e) {
   }
 }
 
-// dom calls
-document.querySelector('form').addEventListener('submit', find)
 // after first time submit doesn't work
 document.querySelector('#search').addEventListener('keydown', submit)
